7f4c2e7da9a07bf688e46e02f9d2d16f4c1f7641
FJ9019 <jfenelus@jfenelus.com>
2023-11-08 10:56:02 -0500

    Swiper JS

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ let menuBar = document.querySelector('#menuBtn');
 menuBar.onclick = () => {
     navBar.classList.toggle('active');
 }
+
+
+/*== Scroll Section And Sticky Navbar ==*/
+window.onscroll = () => {
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+}
+
+/*== Swiper JS ==*/
+    var swiper = new Swiper(".myHome", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      loop: true,
+      autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
