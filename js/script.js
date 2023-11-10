ecefd2c2f5c807b2bfe29505a1e6362ebf68a5ab
FJ9019 <jfenelus@jfenelus.com>
2023-11-10 09:01:13 -0500

    Counter JS

--- js/script.js
+++ js/script.js
@@ -33,3 +33,22 @@ window.onscroll = () => {
         prevEl: ".swiper-button-prev",
       },
     });
+
+/*== Counter Section Start ==*/
+    let valueDisplay = document.querySelectorAll('.num');
+    let interval = 1000;
+
+    valueDisplay.forEach((valueDisplay) => {
+
+        let startValue = 0;
+        let endValue = parseInt(valueDisplay.getAttribute("data-value"));
+
+        let duration = Math.floor(interval/endValue);
+        let counter = setInterval(function () {
+            startValue += 1;
+            valueDisplay.textContent = startValue;
+            if(startValue == endValue) {
+                clearInterval(counter);         
+            }
+        }, duration);
+    });
